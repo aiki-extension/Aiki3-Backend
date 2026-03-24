fca512b15d4f328324551a315e1380e982146f1e
DragonFlyersx <Mathiasl2@hotmail.com>
2026-03-24 12:27:35 +0100

    Update learningController.ts
    Updated the controller to find the user from req.user.id instead of the request body

--- src/controllers/learningController.ts
+++ src/controllers/learningController.ts
@@ -2,9 +2,11 @@ import type { FastifyRequest, FastifyReply } from "fastify";
 import prisma from "../lib/prisma.js";
 import { toUserLearningSiteDto, type UserLearningSiteDto } from "../dtos/UserLearningSiteDto.js";
 
-// GET /api/learningsites
+// GET /api/learningsite
 export async function getLearningsiteById(req: FastifyRequest, reply: FastifyReply) {
-  const { userId } = req.body as { userId: string };
+  const userId = req.user.id;
+
+  // Fetch the learning site for the user, including the related website information
 
   const learningsite = await prisma.userLearningSite.findUnique({
     where: {
@@ -13,21 +15,22 @@ export async function getLearningsiteById(req: FastifyRequest, reply: FastifyRep
     include: { website: true },
   });
 
+  // If no learning site is found for the user, return a 404 error
   if (!learningsite) {
     return reply.status(404).send({ message: "Learning site not found" });
   }
 
-
-
+  // sends the learning site as a DTO
   const dto: UserLearningSiteDto = toUserLearningSiteDto(learningsite);
   return reply.send(dto);
 }
 
 
 
-// POST /api/learningsites
+// POST /api/learningsite
 export async function createLearningsite(req: FastifyRequest, reply: FastifyReply) {
-  const { userId, domain } = req.body as { userId: number; domain: string };
+  const { domain } = req.body as { domain: string };
+  const userId = req.user.id;
 
   // Check if website exists, create if not
   const website = await prisma.website.upsert({
@@ -39,7 +42,7 @@ export async function createLearningsite(req: FastifyRequest, reply: FastifyRepl
   // Creates the learning site entry, linking the user and the website
   const learningsite = await prisma.userLearningSite.create({
     data: {
-      userId,
+      userId: Number(userId),
       websiteId: website.id,
     },
     include: { website: true },
