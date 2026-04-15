2b3fdb6d777ea48cb652683b772ad451c355693f
Nikolaj Carlson <nhca@itu.dk>
2026-04-15 16:44:01 +0200

    FeatureToggles file added
    This file contains all the features that will be set as feature flags (for now only the redirect prompt). It then contains a map consisting of all of them, which will result in a list called "flags", which will be sent through with the response. Such that the frontend receives a list containing all features with an attached boolean, to determine if the feature should be enabled or not
    
    Co-Authored-By: Nikolaj <69769473+niko391a@users.noreply.github.com>

---
+++ src/services/FeatureToggles.ts
@@ -0,0 +1,15 @@
+import type { User } from '../generated/prisma/client.js';
+import prisma from '../lib/prisma.js';
+
+type InviteCode = { code: string } | null;
+
+const redirectPrompt = (inviteCode: InviteCode): boolean => {
+    // Return true only if user has the REDIRECT-CODE (Code to be changed later)
+    return inviteCode?.code === 'REDIRECT-CODE';
+}
+
+// Map containing all features for feature flags
+export const featureMap = (inviteCode: InviteCode) => ({
+    redirectPrompt: redirectPrompt(inviteCode),
+});
+
